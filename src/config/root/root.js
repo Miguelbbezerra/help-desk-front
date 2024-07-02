f0389504e01f2b2b8f046e575c037c8f94c7ea53
carlos miguel <carlosmiguelcm820@gmail.com>
2024-07-02 14:33:41 -0400

    add config and drawer

---
+++ src/config/root/root.js
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+import PersistentDrawerLeft from "../../layout/persistentDrawer";
+
+export default function Root() {
+    return (
+        <PersistentDrawerLeft>
+            <Outlet />
+        </PersistentDrawerLeft>
+    )
+}
--- src/config/route/index.js
+++ src/config/route/index.js
@@ -1,7 +1,8 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Root from "../root/root";
 
 export const router = createBrowserRouter(createRoutesFromElements(
-    <Route>
+    <Route element={<Root />}>
         <Route path="/" element={<h1>ROTA OK</h1>}></Route>
         <Route path="/login" element={<h1>ROTA OK</h1>}></Route>
     </Route>
